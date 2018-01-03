b0e9617bf2a3901a8c3df276398eecd065edde14
Vlad Holubiev <golubev.vld@gmail.com>
2018-01-03 21:42:07 +0200

    docs: add jsdoc

--- src/move-docs.js
+++ src/move-docs.js
@@ -1,3 +1,13 @@
+/**
+ * Move documents from one to another mongo collection
+ * @param {Object} options Options to define the move
+ * @param {Object} options.fromCollection source collection - MongoClient collection object
+ * @param {Object} options.toCollection destination collection - MongoClient collection object
+ * @param {Object} [options.selector={}] Query selector to filter documents to move
+ * @param {Object} [options.projection={}] Query projection to move only specific fields
+ * @param {Function} [options.transformerFn] Function to apply transformations over documents before moving
+ * @param {Number} [options.chunkSize=1000] Bulk operation chunk size to split move
+ */
 export default async function({fromCollection, toCollection, selector = {}, projection = {}, transformerFn = (d) => d, chunkSize = 1000}) {
   let fromCollectionBulk = fromCollection.initializeUnorderedBulkOp();
   let toCollectionBulk = toCollection.initializeUnorderedBulkOp();
